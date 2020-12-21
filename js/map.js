87c08653d2a06aa4f3949a857fe2092bb74df00c
emuzem <alinasklar1@gmail.com>
2020-12-21 13:15:51 +0200

    все работает как надо, переходим к реализации пост и гет запросов

--- js/map.js
+++ js/map.js
@@ -8,39 +8,19 @@ let featuresList = ["wifi", "dishwasher", "parking", "washer", "elevator", "cond
 let photosList = ["http://o0.github.io/assets/images/tokyo/hotel1.jpg",
     "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
     "http://o0.github.io/assets/images/tokyo/hotel3.jpg"];
+
 // обьявление переменных
 const map = document.querySelector('.map');
-let pins = document.querySelector('.map__pins');
+const pins = document.querySelector('.map__pins');
 const fieldset = document.querySelectorAll('fieldset');
 const noticeForm = document.querySelector('.notice__form');
 const mapPinMain = document.querySelector('.map__pin--main');
 const coordinates = {};
 const inputAddress = document.getElementById('address');
-const popupClose = document.querySelector('.popup__close');
-const popup = document.querySelector('.popup');
+
 // генерирование списка предложений
 const offersList = [];
 renderOfferList(8);
-
-function showPins() {
-    let pinFragment = document.createDocumentFragment();
-    for (let i = 0; i < 8; i++) {
-        pinFragment.append(fillTemplate(offersList[i], i));
-    }
-    pins.append(pinFragment);
-}
-
-function showPopup(i) {
-    let popupFragment = document.createDocumentFragment();
-    popupFragment.append(fillPopup(offersList[i]));
-    map.append(popupFragment);
-}
-
-function hidePopup () {
-    popup.classList.add('hidden');
-}
-
-//popupClose.addEventListener('click', showPopup);
 
 //блокировка и активация форм
 disableForms();
@@ -50,20 +30,98 @@ mapPinMain.addEventListener('mouseup', (ev)=> {
     inputAddress.value = `${coordinates.x}, ${coordinates.y}`;
     showPins();
 });
+function disableForms (){
+    fieldset.forEach((el) => {
+        el.setAttribute('disabled', 'disabled');
+    });
+}
+function ableForms (){
+    fieldset.forEach((el) => {
+        el.removeAttribute('disabled');
+    })
+    noticeForm.classList.remove('notice__form--disabled');
+    map.classList.remove('map--faded');
+}
 
-// функции
+//отрисовка пинов
+function showPins() {
+    let pinFragment = document.createDocumentFragment();
+    for (let i = 0; i < 8; i++) {
+        pinFragment.append(fillTemplate(offersList[i], i));
+    }
+    pins.append(pinFragment);
+}
+
+//отрисовка попапа при вызове
+function showPopup(i) {
+    let popupFragment = document.createDocumentFragment();
+    popupFragment.append(fillPopup(offersList[i]));
+    map.append(popupFragment);
+    const popupClose = document.querySelector('.popup__close');
+    const popup = document.querySelectorAll('.popup');
+    function hidePopup (ev){
+        if (ev.target === popupClose) {
+            popup.forEach(el => map.removeChild(el));
+            document.addEventListener('click', hidePopup);
+        }
+    }
+    document.addEventListener('click', hidePopup);
+}
+
+//заполнение шаблона пина и отрисовка попапа при нажатии
 function fillTemplate (obj, i){
     let pinClone = document.querySelector("template").content.querySelector('.map__pin').cloneNode(true);
 pinClone.querySelector('img').src = obj.author.avatar;
 pinClone.style = `left: ${obj.location.x}px; top: ${obj.location.y}px;`;
 pinClone.querySelector('img').alt = obj.offer.title;
+
 pinClone.addEventListener('click', ()=> {
     showPopup(i);
-   // popup.classList.remove('hidden');
+    document.removeEventListener('click', hidePopup);
 });
 return pinClone;
 }
 
+//заполнение информации внутри попапа
+function fillPopup (obj){
+    let popupClone = document.querySelector('template').content.querySelector('.map__card').cloneNode(true);
+    popupClone.querySelector('.popup__title').textContent = obj.offer.title;
+    popupClone.querySelector('.popup__text--address').textContent = obj.offer.address;
+    popupClone.querySelector('.popup__text--price').textContent = `${obj.offer.price}₽/ночь`;
+    popupClone.querySelector('.popup__type').textContent = obj.offer.type;
+    popupClone.querySelector('.popup__text--capacity').textContent = `${obj.offer.rooms} комнаты для ${obj.offer.guests} гостей`;
+    popupClone.querySelector('.popup__text-time').textContent = `Заезд после ${obj.offer.checkin}, выезд до ${obj.offer.checkout}`;
+    showFeatures(obj.offer.features, popupClone);
+    popupClone.querySelector('.popup__description').textContent = obj.offer.description;
+    showPhotos(obj.offer.photos, popupClone);
+    popupClone.querySelector('.popup__avatar').src = obj.author.avatar;
+    return popupClone;
+}
+function showFeatures (feature, popupClone){
+    let ul = popupClone.querySelector('.popup__features');
+    for(let i = 0; i < feature.length; i++){
+        let liElem = document.createElement('li');
+        liElem.classList.add('feature');
+        liElem.classList.add(`feature--${feature[i]}`);
+        ul.appendChild(liElem);
+    }
+}
+function showPhotos (pics, popupClone){
+    let ul = popupClone.querySelector('.popup__pictures');
+    for(let i = 0; i < pics.length; i++){
+        let photo = document.createElement('img');
+        photo.src = pics[i];
+        photo.style = 'width: 60px; height: 60px; padding: 2px;';
+        ul.appendChild(photo);
+    }
+}
+function findCoordinates(ev){
+    coordinates.x = ev.screenX;
+    coordinates.y = ev.screenY + 50;
+    return coordinates;
+}
+
+//генерация самого массива данных
 function renderOfferList (number){
     for (let i = 0; i < number; i++){
         offersList[i] = {
@@ -92,13 +150,6 @@ function renderOfferList (number){
     }
     return offersList;
 }
-
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-}
-
 function generateFeaturesList (){
     let array = [];
     let number = getRandomIntInclusive(1, 5);
@@ -108,57 +159,9 @@ function generateFeaturesList (){
     return array;
 }
 
-function showFeatures (feature, popupClone){
-    let ul = popupClone.querySelector('.popup__features');
-    for(let i = 0; i < feature.length; i++){
-                let liElem = document.createElement('li');
-                liElem.classList.add('feature');
-                liElem.classList.add(`feature--${feature[i]}`);
-                ul.appendChild(liElem);
-    }
-}
-
-function showPhotos (pics, popupClone){
-    let ul = popupClone.querySelector('.popup__pictures');
-    for(let i = 0; i < pics.length; i++){
-        let photo = document.createElement('img');
-        photo.src = pics[i];
-        photo.style = 'width: 60px; height: 60px; padding: 2px;';
-        ul.appendChild(photo);
-    }
-}
-
-function fillPopup (obj){
-    let popupClone = document.querySelector('template').content.querySelector('.map__card').cloneNode(true);
-    popupClone.querySelector('.popup__title').textContent = obj.offer.title;
-    popupClone.querySelector('.popup__text--address').textContent = obj.offer.address;
-    popupClone.querySelector('.popup__text--price').textContent = `${obj.offer.price}₽/ночь`;
-    popupClone.querySelector('.popup__type').textContent = obj.offer.type;
-    popupClone.querySelector('.popup__text--capacity').textContent = `${obj.offer.rooms} комнаты для ${obj.offer.guests} гостей`;
-    popupClone.querySelector('.popup__text-time').textContent = `Заезд после ${obj.offer.checkin}, выезд до ${obj.offer.checkout}`;
-    showFeatures(obj.offer.features, popupClone);
-    popupClone.querySelector('.popup__description').textContent = obj.offer.description;
-    showPhotos(obj.offer.photos, popupClone);
-    popupClone.querySelector('.popup__avatar').src = obj.author.avatar;
-    return popupClone;
-}
-
-function disableForms (){
-    fieldset.forEach((el) => {
-        el.setAttribute('disabled', 'disabled');
-    });
-}
-
-function ableForms (){
-    fieldset.forEach((el) => {
-        el.removeAttribute('disabled');
-    })
-    noticeForm.classList.remove('notice__form--disabled');
-    map.classList.remove('map--faded');
-}
-function findCoordinates(ev){
-    coordinates.x = ev.screenX;
-    coordinates.y = ev.screenY + 50;
-    return coordinates;
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
 }
 
